3fc0b7aede7d5d5dae6b981b6dbfa71b45174ad3
Jessica Bader <jabader@iastate.edu>
2018-02-05 14:35:09 -0600

    Added lastName, email, username, password, and confirmPassword to New_Profile

--- app/src/main/java/com/conectar/conectar/New_Profile.java
+++ app/src/main/java/com/conectar/conectar/New_Profile.java
@@ -2,12 +2,24 @@ package com.conectar.conectar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class New_Profile extends AppCompatActivity {
-
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText username;
+    private EditText password;
+    private EditText confirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new__profile);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
     }
 }
