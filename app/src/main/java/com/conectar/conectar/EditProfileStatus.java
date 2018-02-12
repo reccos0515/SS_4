d35c18d5667828699d39aa4047312e007044ae29
Jessica Bader <jabader@iastate.edu>
2018-02-12 13:26:33 -0600

    Update edit profile, input for bio and adding an interest.  Adding interest crashes currently

--- app/src/main/java/com/conectar/conectar/EditProfileStatus.java
+++ app/src/main/java/com/conectar/conectar/EditProfileStatus.java
@@ -1,20 +1,42 @@
 package com.conectar.conectar;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class EditProfileStatus extends AppCompatActivity {
-
+    private EditText bio;
+    private EditText newInterest;
+    private ArrayList<String> interests;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile_status);
-
-        Spinner spinner = (Spinner) findViewById(R.id.activityStatus);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.activityStatus, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        bio = findViewById(R.id.bio);
+        newInterest = findViewById(R.id.newInterest);
+        Button button = findViewById(R.id.submitInterest);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(interests.size() < 6){
+                    interests.add(newInterest.getText().toString());
+                }
+                else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Interests are full";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
     }
 }
