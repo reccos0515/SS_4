a49811320decccb34b12b13bd40a66f9d7adeaba
Jessica Bader <jabader@iastate.edu>
2018-02-12 15:50:54 -0600

    Can successfully delete the first interest

--- app/src/main/java/com/conectar/conectar/EditProfileStatus.java
+++ app/src/main/java/com/conectar/conectar/EditProfileStatus.java
@@ -70,63 +70,15 @@ public class EditProfileStatus extends AppCompatActivity {
                 }
             }
         });
-        //button to delete interest 2
-        Button button2 = findViewById(R.id.interestTwo);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button debugButton = findViewById(R.id.interestFive);
+        debugButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //check there is an interest 2
-                if(numInterests >= 2){
-                    //move every interest from the second interest up, down one place
-                    for(int i = 1; i < numInterests; i++){
-                        interests[i] = interests[i + 1];
-                    }
-                    //decrement number of interests
-                    numInterests--;
-                }
-            }
-        });
-        //button to delete interest 3
-        Button button3 = findViewById(R.id.interestThree);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //check there is an interest 3
-                if(numInterests >= 3){
-                    //move every interest from the third interest up, down one place
-                    for(int i = 2; i < numInterests; i++){
-                        interests[i] = interests[i + 1];
-                    }
-                    //decrement number of interests
-                    numInterests--;
-                }
-            }
-        });
-        //button to delete interest 4
-        Button button4 = findViewById(R.id.interestFour);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //check there is an interest 4
-                if(numInterests >= 4){
-                    //move every interest from the fourth interest up, down one place
-                    for(int i = 3; i < numInterests; i++){
-                        interests[i] = interests[i + 1];
-                    }
-                    //decrement number of interests
-                    numInterests--;
-                }
-            }
-        });
-        //button to delete interest 5
-        Button button5 = findViewById(R.id.interestFive);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //set the fifth interest to be null
-                interests[5] = null;
-                //decrement number of interests
-                numInterests--;
+                Context context = getApplicationContext();
+                CharSequence text = (CharSequence)(interests[0] + interests[1] + interests[2] + interests[3] + interests[4]);
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
