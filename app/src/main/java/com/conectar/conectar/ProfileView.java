96960c7c6b8fad6eba08a10e6eb6d437e9aa7ddf
Jessica Bader <jabader@iastate.edu>
2018-02-13 21:51:46 -0600

    Profile view for the second demo.  Can view a customizable username and has button to add friend which is not yet implemented

--- app/src/main/java/com/conectar/conectar/ProfileView.java
+++ app/src/main/java/com/conectar/conectar/ProfileView.java
@@ -2,6 +2,7 @@ package com.conectar.conectar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileView extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class ProfileView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+        //text to be set to the username
+        String text = "trial";
+        //edit this to set the username
+        TextView username = findViewById(R.id.viewUsername);
+        username.setText(text);
     }
 }
