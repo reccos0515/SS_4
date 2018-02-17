8c3ce24ba1067a16fbae44a3bb8ab5aa65521e80
IASTATE\basimon <basimon@iastate.edu>
2018-02-17 10:59:22 -0600

    creating local server

---
+++ coNectar_localserver/src/main/java/co/nectar/CoNectarLocalserverApplication.java
@@ -0,0 +1,12 @@
+package co.nectar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoNectarLocalserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoNectarLocalserverApplication.class, args);
+	}
+}
