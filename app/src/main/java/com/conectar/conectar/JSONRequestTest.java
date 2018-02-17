2221c9d56a271a3f7d331518df8e5995274c07bb
Jessica Bader <jabader@iastate.edu>
2018-02-17 10:36:46 -0600

    Create a class to practice sending JSON request in order to figure out/debug the process of server communication

---
+++ app/src/main/java/com/conectar/conectar/JSONRequestTest.java
@@ -0,0 +1,100 @@
+package com.conectar.conectar;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link JSONRequestTest.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link JSONRequestTest#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ * This class is for testing purposes, to learn how to send a JSON request.  It will be deleted later
+ */
+public class JSONRequestTest extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public JSONRequestTest() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment JSONRequestTest.
+     */
+    public static JSONRequestTest newInstance() {
+        JSONRequestTest fragment = new JSONRequestTest();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jsonrequest_test, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.JSONrequest).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               //TODO send request
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
