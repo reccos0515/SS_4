649000c982d9b3cc77243e232787950479159df9
Maggie <mjdalton@iastate.edu>
2018-02-26 20:03:48 -0600

    added more methods and comments to user object classes

--- app/src/main/java/util/CurrentUser.java
+++ app/src/main/java/util/CurrentUser.java
@@ -1,6 +1,9 @@
 package util;
 
 
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -9,11 +12,21 @@ import org.json.JSONObject;
  */
 
 public class CurrentUser extends User {
-    boolean isLoggedIn;
+
+    private boolean isLoggedIn;
+    private String[] interests;
+    private String userEmail;
 
     public CurrentUser(JSONObject js) {
         super(js);
         isLoggedIn = true;
+        try {
+            userEmail = js.getString("email");
+            //set interests from jsonobject
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
@@ -23,13 +36,59 @@ public class CurrentUser extends User {
         //logs the user out
         if(isLoggedIn){
             isLoggedIn = false;
-            //remove values from other variables
+            //remove values from other variables?
         }
     }
 
-    public void setBio(){
+    /**
+     * Set the user's local variable for bio
+     * @param bio a string of the typed bio from the user
+     */
+    public void setBio(String bio){
         //set the user's session variable for bio
         //post to server from here????
+    }
+
+    /**
+     * Sets the user's local variable for interests
+     * @param interests an array of selected interests
+     */
+    public void setInterests(String[] interests){
+        //do things
+        //post to server from here?
+    }
+
+    /**
+     * gets a list of user interests
+     * @return an array of user interests
+     */
+    public String[] getInterests(){
+        return interests;
+    }
+
+    /**
+     * A list of users the current user is friends with
+     * @return the list of friends
+     */
+    public User[] getFriends(){ //TODO figure out whether or not to use User[] or String[] of ids
+        //get a list of friends from server
+        return null;
+    }
+
+    /**
+     * Sends a friend request
+     * @param id the user the request will be sent to
+     */
+    public void makeFriend(String id){
+
+    }
+
+    /**
+     * Removes a user from current user's friend list
+     * @param id the user to be removed
+     */
+    public void removeFriend(String id){
+
     }
 
 
--- app/src/main/java/util/User.java
+++ app/src/main/java/util/User.java
@@ -6,14 +6,16 @@ import org.json.JSONObject;
 /**
  * Created by Maggie on 2/26/2018.
  * Superclass for user objects
+ * Currently logged in user's variables will be stored in CurrentUser object, all other users will
+ * be manipulated as regular Users
  */
 
 public class User {
 
-    String userName;
-    String userId;
-    String userPassword;
-    String userBio;
+    private String userName; //idk if these should be private, yell at me if not
+    private String userId;
+    private String userPassword;
+    private String userBio;
 
     public User(JSONObject js){
         //parse jsonobject into fields
@@ -27,15 +29,34 @@ public class User {
         }
     }
 
+    /**
+     * Getter for user's username
+     * @return the user's username
+     */
     public String getUsername(){
         return userName;
     }
+
+    /**
+     * Getter for user id
+     * @return the user's id
+     */
     public String getUserId(){
         return userId;
     }
+
+    /**
+     * Getter for user's password
+     * @return password
+     */
     public String getPassword(){
         return userPassword;
     }
+
+    /**
+     * Getter for user's bio
+     * @return user's bio
+     */
     public String getBio(){
         return userBio;
     }
