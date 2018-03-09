5b378afa9a933eddd56f283cf66545cecf67abda
Maggie <mjdalton@iastate.edu>
2018-03-08 18:37:26 -0600

    More methods for session variable management

--- app/src/main/java/util/SessionUtil.java
+++ app/src/main/java/util/SessionUtil.java
@@ -1,9 +1,12 @@
 package util;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 
 import org.json.JSONArray;
+
+import java.util.HashMap;
 
 /**
  * Created by Maggie on 3/8/2018.
@@ -33,11 +36,40 @@ public class SessionUtil {
         editor = preferences.edit();
     }
 
-    public void createLoginSession(String username, String id, String status){
+    /**
+     * Creates a new session and places a user's session variables in sharedPreferences
+     * @param username the user's username
+     * @param id the user's id number
+     * @param status the user's current status
+     */
+    public void createSession(String username, String id, String status){
         editor.putBoolean(IS_LOGIN, true);
         editor.putString(KEY_USERNAME, username);
         editor.putString(KEY_ID, id);
         editor.putString(KEY_STATUS, status);
+        editor.apply();
     }
+
+    /**
+     * Destroys a user's session variables in sharedPreferences
+     */
+    public void destroySession(){
+        editor.clear();
+        editor.commit();
+    }
+
+    /**
+     * Returns map of all of a user's session variables for their information from
+     * sharedPreferences
+     * @return a hashmap of the details
+     */
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        user.put(KEY_USERNAME, preferences.getString(KEY_USERNAME, null));
+        user.put(KEY_ID, preferences.getString(KEY_ID, null));
+        user.put(KEY_STATUS, preferences.getString(KEY_STATUS, null));
+        return user;
+    }
+
 
 }
