a77deb4e27315f01c334219361bd86245dbc3ede
Jessica Bader <jabader@iastate.edu>
2018-03-17 21:46:24 -0500

    Start of the class to convert from numbers to an activity

---
+++ app/src/main/java/util/Interests.java
@@ -0,0 +1,90 @@
+package util;
+
+/**
+ * Created by jabad on 3/17/2018.
+ * Takes a 2 digit string of 2 ints and returns the string of the corresponding activity
+ */
+
+public class Interests {
+
+    public String getInterest(String i){
+        if(i.equals("01")){
+            return "Snowboarding";
+        }
+        else if(i.equals("02")){
+            return "Movies";
+        }
+        else if(i.equals("03")){
+            return "Hammocking";
+        }
+        else if(i.equals("04")){
+            return "Volunteering";
+        }
+        else if(i.equals("05")){
+            return "Video Games";
+        }
+        else if(i.equals("06")){
+            return "Board Games";
+        }
+        else if(i.equals("07")){
+            return "Shopping";
+        }
+        else if(i.equals("08")){
+            return "Bowling";
+        }
+        else if(i.equals("09")){
+            return "Ice Skating";
+        }
+        else if(i.equals("10")){
+            return "Roller Blading";
+        }
+        else if(i.equals("11")){
+            return "Laser Tag";
+        }
+        else if(i.equals("12")){
+            return "Painting pottery";
+        }
+        else if(i.equals("13")){
+            return "Getting lunch";
+        }
+        else if(i.equals("14")){
+            return "Getting dinner";
+        }
+        else if(i.equals("15")){
+            return "Getting breakfast";
+        }
+        else if(i.equals("16")){
+            return "Getting ice cream";
+        }
+        else if(i.equals("17")){
+            return "Going to concerts";
+        }
+        else if(i.equals("18")){
+            return "Going to the park";
+        }
+        else if(i.equals("19")){
+            return "Hiking";
+        }
+        else if(i.equals("20")){
+            return "Biking";
+        }
+        else if(i.equals("21")){
+            return "Skateboarding";
+        }
+        else if(i.equals("22")){
+            return "Going to museums";
+        }
+        else if(i.equals("23")){
+            return "Jogging";
+        }
+        else if(i.equals("24")){
+            return "Swing dancing";
+        }
+        else if(i.equals("25")){
+            return "Square dancing";
+        }
+        else{
+            return null;
+        }
+    }
+}
