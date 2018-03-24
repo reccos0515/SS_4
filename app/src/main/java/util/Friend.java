6cc439846829cc9ec2566fb259f88683d25a0f0b
Maggie <mjdalton@iastate.edu>
2018-03-24 09:57:57 -0500

    Method for grabbing array of friends objects for a user for use by the adapter for the friends list

--- app/src/main/java/util/Friend.java
+++ app/src/main/java/util/Friend.java
@@ -1,9 +1,15 @@
 package util;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -17,6 +23,8 @@ public class Friend {
     private String username;
     private String bio;
     private String interests;
+    private JSONObject js = new JSONObject();
+    private JSONArray users = new JSONArray();
 
     public Friend(){
         super();
@@ -30,6 +38,18 @@ public class Friend {
         this.interests = interests;
     }
 
+    public Friend(JSONObject js){
+        super();
+        try {
+            this.bio = js.getString("bio");
+            this.id = js.getString("id");
+            this.username = js.getString("userName");
+            this.interests = js.getString("interests");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * Lists all of the information regarding a Friend object
      * @return string of all Friend object attributes
@@ -39,11 +59,55 @@ public class Friend {
     }
 
     public Friend[] getFriends(String id, Context context){
-        String url = "";
+        String url = "https://proj-309-ss-4.cs.iastate.edu:9002/ben/users/" + id + "/friends";
         Friend[] friends = null;
+        String success = "";
+        String message = "";
 
-        //JsonObjectRequest = new JsonObjectRequest()
+        //get the object with the list of friends within it
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                js = response;
+                Log.d("Friend", "getFriends response from GET: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
+            }
+        });
+        Singleton.getmInstance(context).addToRequestQueue(jsonObjectRequest);
+
+        try {
+            success = js.getString("success");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(success.equals("success")){ //if the request was successful
+            try {
+                users = js.getJSONArray("users"); //get JSONArray of all users the user is friends with
+                friends = new Friend[users.length()]; //makes the array of friends the correct size
+                Log.d("Friend", "Grabbing users, looks like: " + users.toString());
+                for(int i = 0; i < users.length(); i++){ //grab all friends out of the array
+                    Friend newFriend = new Friend(users.getJSONObject(i)); //convert them to friend objects
+                    friends[i] = newFriend; //store them in friends
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else{ //if the server returns an error, display it in logs
+            try {
+                js.getString("message");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.d("Friend", "getFriends error message: " + message);
+        }
+
+        Log.d("Friend", "List of friends: " + friends.toString());
         return friends;
     }
 
