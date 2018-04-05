c7b0d2245c821308466879a01e694dba5574a9ae
Tristan Anderson <atristan@iastate.edu>
2018-04-05 00:28:55 -0500

    Ugly and early works on the admin/mod page. I'm sayin real ugly

---
+++ coNectar_sqlserver/src/main/java/co/nectar/webpage/GreetingController.java
@@ -0,0 +1,20 @@
+package co.nectar.webpage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(path = "/web")
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
