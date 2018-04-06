37d197404b67d535681e5e9e5d2f18a6a486f3f2
Jessica Bader <jabader@iastate.edu>
2018-04-06 11:41:17 -0500

    Implementation of user. Some implementation of message

--- app/src/main/java/messaging/Message.java
+++ app/src/main/java/messaging/Message.java
@@ -1,6 +1,7 @@
 package messaging;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.ImageView;
 
 /**
@@ -9,16 +10,26 @@ import android.widget.ImageView;
 
 public class Message {
 
+    private static Context mContext;
+    private static String time;
+
     public static String getMessage(){ //TODO implement
         return null;
     }
 
 
+    /**
+     * need to send context first with setContext()
+     * @return
+     */
     public static User getSender(){//TODO implement
-        return null;
+        //Set up shared preferences, has to be done within onViewCreated otherwise it will throw all sorts of null pointer exceptions
+        final SharedPreferences preferences = mContext.getSharedPreferences("coNECTAR", Context.MODE_PRIVATE); //grabs the sharedpreferences for our session (labeled coNECTAR)
+        User user = new User(preferences.getString("USERNAME", ""), "", preferences.getInt("ID", 0));
+        return user;
     }
 
-    public static Message get(int position){//TODO implement
+    public static Message get(int position){//TODO implement (???)
         return null;
     }
 
@@ -27,11 +38,16 @@ public class Message {
     }
 
     public static String getCreatedAt(){
-        return null;
+        return time;
     }
+
 
     public static void displayRoundImageFromUrl(Context context, String url, ImageView profileImage){
 
+    }
+    public static void setContext(Context context){
+        mContext = context;
+        return;
     }
 
 
--- app/src/main/java/messaging/User.java
+++ app/src/main/java/messaging/User.java
@@ -1,20 +1,46 @@
 package messaging;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Maggie on 4/5/2018.
  */
 
 public class User {
+    private static String username;
+    private static String profileURL;
+    private static int id;
 
-    public static String getUsername(){//TODO implement
-        return null;
+    public User(){
+        super();
     }
 
-    public static String getProfileUrl(){//TODO implement
-        return null;
+    public User(JSONObject user){
+        try{
+            username = user.getString("userName");
+            id = user.getInt("id");
+            //todo add profile url
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 
-    public static int getUserId(){//TODO implement
-        return 0;
+    public User(String username, String profileURL, int id){
+        this.username = username;
+        this.profileURL = profileURL;
+        this.id = id;
+    }
+
+    public static String getUsername(){
+        return username;
+    }
+
+    public static String getProfileUrl(){
+        return profileURL;
+    }
+
+    public static int getUserId(){
+        return id;
     }
 }
