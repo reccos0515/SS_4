4b21a9b50a071cfa0d25093bd39d2040632acaee
Maggie <mjdalton@iastate.edu>
2018-04-05 21:43:46 -0500

    Added toast for unsuccessful login attempts, fixed stuff that made app crash from messages

--- app/src/main/java/com/conectar/conectar/LoginFragment.java
+++ app/src/main/java/com/conectar/conectar/LoginFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -124,6 +125,9 @@ public class LoginFragment extends Fragment {
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     fragmentTransaction.replace(R.id.screen_area, fragment);
                     fragmentTransaction.commit();
+                }
+                else{
+                    Toast.makeText(getActivity(), "Login Unsuccessful", Toast.LENGTH_LONG).show();
                 }
 
 
