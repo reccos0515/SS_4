d27600375d2dcf67fe1f0f4a33a2dad217fcbb15
Maggie Dalton <mjdalton@iastate.edu>
2018-04-10 11:49:21 -0500

    Added package information for our packages

---
+++ app/src/main/java/messaging/package-info.java
@@ -0,0 +1,4 @@
+/**
+* Messages is a set of helper methods and objects used to facilitate messaging.  These methods/objects enable messaging to utilize a unique view for the display of messages between users and make Android Vollley calls to the package co.nectar.message.  These objects also define what a message contains and execute logic for the preparation/display of conversations.  Many of the methods/objects are used within com.conectar.conectar.
+*/
+package messaging;
