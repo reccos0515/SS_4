8f97d73f688b00d803a33db43f13d51f3c267add
basimon <basimon@iastate.edu>
2018-04-15 22:23:03 -0500

    Revert "Revert "fixed yellow user bug, and added test user function""
    This reverts commit f87ba9cb113bf62eef50f105cdf3657e3867c073.

--- coNectar_sqlserver/src/main/java/co/nectar/login/LoginController.java
+++ coNectar_sqlserver/src/main/java/co/nectar/login/LoginController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.nectar.HtmlResponce.HtmlError;
+import co.nectar.HtmlResponce.HtmlResponce;
+
 
 
 
@@ -22,6 +25,12 @@ public class LoginController {
 		return login;	
 	}
 	
+	//add test users
+	@RequestMapping("/login/test")
+	public HtmlResponce addTestLogins() {
+		loginService.addTestUsers();
+		return new HtmlError(true,"");	
+	}
 	
 	/**
 	 * adds login to login list
