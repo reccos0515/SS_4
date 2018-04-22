61c092065204cf8a5034c5d85c1873d9c74e1289
Maggie <mjdalton@iastate.edu>
2018-04-22 16:31:03 -0500

    Updated PendingFriendsAdapter, just have to set

--- app/src/main/java/friends/PendingFriendsAdapter.java
+++ app/src/main/java/friends/PendingFriendsAdapter.java
@@ -1,33 +1,58 @@
 package friends;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.conectar.conectar.R;
+
+import util.UserUtil;
 
 public class PendingFriendsAdapter extends BaseAdapter{
+    Context context;
+    Friend[] pendingFriends;
+    private static LayoutInflater inflater = null;
 
-    public PendingFriendsAdapter(Context context){
 
+    public PendingFriendsAdapter(Friend[] pendingFriends, Context context){
+        this.context = context;
+        this.pendingFriends = pendingFriends;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return pendingFriends.length;
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Object getItem(int position) {
+        return pendingFriends[position];
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        View newView = view;
+        if(newView == null){
+            newView = inflater.inflate(R.layout.pending_list_item, null);
+            TextView username = (TextView) newView.findViewById(R.id.pendingUsernameView);
+            ImageView profilePicture = (ImageView) newView.findViewById(R.id.profilePic);
+
+            Friend thisPending = pendingFriends[i];
+            username.setText(thisPending.getUsername());
+            int profilePicNum = thisPending.getProfilePicture();
+            UserUtil.updateProfilePicture(profilePicNum, profilePicture);
+        }
+
+        return newView;
     }
 }
